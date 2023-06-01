17a86d2034558b88de881c5e9fa76bd089497bde
Pedro Monteiro <pedromont436@gmail.com>
2023-06-01 15:33:56 -0300

    Library component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,5 @@
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
-  return (
-    <div className="text-green-500">
-      <Sidebar>S</Sidebar>
-    </div>
-  );
+  return <div className="text-green-500">Main content</div>;
 }
