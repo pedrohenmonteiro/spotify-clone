6380955eb34d73d36d8ff5ed59e2cbfcc9e48867
Pedro Monteiro <pedromont436@gmail.com>
2023-06-01 19:52:06 -0300

    ListItem Component

---
+++ app/components/ListItem.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+export default function ListItem({ image, name, href }: ListItemProps) {
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(href);
+  };
+
+  return (
+    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div className="relative, min-h-[64px] min-w-[64px]"></div>
+    </button>
+  );
+}
